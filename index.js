30900e55e8a2abdb4857b716a881bd3bd06359a7
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-07-29 17:11:34 -0300

    Aula 1: desafio

--- index.js
+++ index.js
@@ -9,6 +9,16 @@ console.log(chalk.rgb(102, 0, 153).bold('Estou Vivo!'));
 const criacaoNode = moment('2009-05-27').fromNow();
 console.log(`Node foi criado ${criacaoNode}.`);
 
+console.log('----');
+
 fs.readFile('por-que-a-vivo.txt', 'utf8', (err, data) => {
-  console.log(data); 
+  data.split('\n').filter(linha => linha.trim().length != 0)
+    .forEach((linha, i) => {
+      if (i % 2 == 0) {
+        console.log(chalk.rgb(102, 0, 153).bold(linha));
+      } else {
+        console.log(chalk.grey.bold(linha));
+      }
+    })
 });
+
