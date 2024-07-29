db21baae960cd87f4d7f52655fba617a56c013e3
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-07-29 16:14:54 -0300

    Aula 2: ES Modules com .mjs

--- criacao-node.mjs
+++ criacao-node.mjs
@@ -1,5 +1,7 @@
-const moment = require('moment');
+import moment from 'moment';
 moment.locale('pt');
 
 const criacaoNode = moment('2009-05-27')
                         .fromNow();
+
+export default criacaoNode;
---
+++ index.mjs
@@ -0,0 +1,13 @@
+import fs from 'fs'; // core
+
+import chalk from 'chalk'; // de terceiro
+
+import criacaoNode from './criacao-node.mjs'; // local
+
+console.log(chalk.rgb(102, 0, 153).bold('Estou na Vivo!'));
+
+console.log(`Node foi criado ${criacaoNode}.`);
+
+fs.readFile('por-que-a-vivo.txt', 'utf8', (err, data) => {
+  console.log(data); 
+});
