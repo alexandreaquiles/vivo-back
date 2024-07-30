eb21794200b3492d31ef62e3c28eb9c2c702d83c
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-07-30 16:31:45 -0300

    Aula 3: async/await

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import fs from 'fs/promises';
 
 const app = express();
 const port = 3000;
@@ -7,7 +8,7 @@ app.get('/', (req, res) => {
   res.send('Estou na Vivo!');
 });
 
-app.get('/api/porque-a-vivo', (req, res) => {
+app.get('/api/porque-a-vivo', async (req, res) => {
   const data = await fs.readFile('por-que-a-vivo.json', 'utf8');
   res.json(data);
 });
