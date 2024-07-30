69a065bc8ed3cb057a29ad719fe47ff9f847bee8
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-07-30 16:20:25 -0300

    Aula 3: endpoint '/api/porque-a-vivo'

--- server.js
+++ server.js
@@ -7,6 +7,15 @@ app.get('/', (req, res) => {
   res.send('Estou na Vivo!');
 });
 
+app.get('/api/porque-a-vivo', (req, res) => {
+  const data =
+    [{
+      title: 'Segreados da Qualidade',
+      description: "Tecnologia de ponta e atendimento eficaz."
+    }];
+  res.json(data);
+});
+
 app.listen(port, () => {
   console.log(`Aplicação rodando em http://localhost:${port}`);
 });
