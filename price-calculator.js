69ead4209073964113e01bf75969f6721c380286
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-08-15 20:06:35 -0300

    Aula 8: refatorando

--- price-calculator.js
+++ price-calculator.js
@@ -1,3 +1,9 @@
 export function calculateAnnualPrice (plan) {
-    return plan.price * 12;
+    const monthlyPrice = plan.price;
+    const offer = plan.offer || 0;
+    if (offer > 0) {
+      const discountedPrice = monthlyPrice - offer;
+      return (discountedPrice * 3) + (monthlyPrice * 9);
+    }
+    return monthlyPrice * 12;
 }
