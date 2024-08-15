2e8769c466d4b5466c5fb0a702937c3dfb42030f
Alexandre Aquiles <alexandre.aquiles@gmail.com>
2024-08-15 19:22:51 -0300

    Aula 8: mais testes com Mocha

--- test/price-calculator.test.js
+++ test/price-calculator.test.js
@@ -19,4 +19,15 @@ describe('Annual price calculator', () => {
 
   });
 
+  it('should calculate annual price with offer for 3 months', () => {
+    const plan = {
+      title: "500 Mega",
+      monthlyPrice: 120,
+      offer: 30
+    };
+    const annualPrice = calculateAnnualPrice(plan);
+    expect(annualPrice).to.equal(1350); // (90 * 3) + (120 * 9) 
+                                        // = 270 + 1080 = 1350
+  });
+
 });
